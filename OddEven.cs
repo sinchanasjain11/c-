138ff0f01d2c57c1ee16681d0709aaf6271bdbc3
sinchanasjain11 <sinchanasjain11@gmail.com>
2025-08-01 11:34:26 +0530

    Add files via upload

---
+++ OddEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    public  class OddEven
+    {
+        public void readnumber(int number)
+        {
+            if (number%2==0)
+            {
+                Console.WriteLine($"{number}is even");
+
+            }
+            else
+            {
+                {
+                    Console.WriteLine($"{number}is odd");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OddEven
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 42;
+            OddEven oddeven = new OddEven();
+            oddeven.readnumber(number);
+        }
+    }
+}
