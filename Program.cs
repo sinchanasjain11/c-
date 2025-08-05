273b608f48e4f2acb5744c53db8c7ca1691782bd
sinchanasjain11 <sinchanasjain11@gmail.com>
2025-08-05 17:59:54 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
-﻿namespace squarenum
+﻿namespace person
 {
     internal class Program
     {
          public static void Main(string[] args)
         {
-            squarenum calc = new squarenum(); // Create object of Calculator class
-
-            Console.Write("Enter a number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            int result = calc.Square(num); // Call the Square method
-
-            Console.WriteLine("Square of {0} is {1}", num, result);
+            Person person1 = new Person();  
+            person1.ReadData();    
+            person1.DisplayData();
         }
     }
 }
---
+++ person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person
+{
+    public class Person
+    {
+            public string? name;
+            public int Age;
+
+            public void ReadData()
+            {
+                Console.Write("Enter name: ");
+                name = Console.ReadLine();
+                Console.Write("Enter age: ");
+                Age = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public void DisplayData()
+            {
+                Console.WriteLine("\n--- Person Details ---");
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + Age);
+            }
+        }
+    }
+
