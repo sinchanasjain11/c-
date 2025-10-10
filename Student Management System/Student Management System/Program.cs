45f9330872f56b3865d946408f0b2cb39b4753c4
Sinchana S <sinchanasjain11@gmail.com>
2025-10-10 12:32:26 +0530

    Add files via upload

---
+++ Student Management System/Student Management System/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Student_Management_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n1.Add  2.Display  3.Search  4.Remove  5.Topper  6.Exit");
+                Console.Write("Enter choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("ID: "); int id = int.Parse(Console.ReadLine());
+                        Console.Write("Name: "); string name = Console.ReadLine();
+                        Console.Write("Marks: "); int marks = int.Parse(Console.ReadLine());
+                        students.Add(new Student { Id = id, Name = name, Marks = marks });
+                        break;
+
+                    case 2:
+                        students.ForEach(s => Console.WriteLine(s));
+                        break;
+
+                    case 3:
+                        Console.Write("Search ID: ");
+                        int sid = int.Parse(Console.ReadLine());
+                        var s1 = students.FirstOrDefault(s => s.Id == sid);
+                        Console.WriteLine(s1 != null ? s1 : "Not found");
+                        break;
+
+                    case 4:
+                        Console.Write("Remove ID: ");
+                        int rid = int.Parse(Console.ReadLine());
+                        students.RemoveAll(s => s.Id == rid);
+                        Console.WriteLine("Removed (if existed).");
+                        break;
+
+                    case 5:
+                        if (students.Count > 0)
+                            Console.WriteLine("Topper: " + students.OrderByDescending(s => s.Marks).First());
+                        else
+                            Console.WriteLine("No students.");
+                        break;
+                }
+            } while (choice != 6);
+        }
+    }
+}
---
+++ Student Management System/Student Management System/Student management.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_System
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public int Marks;
+        public override string ToString() => $"ID:{Id}, Name:{Name}, Marks:{Marks}";
+    }
+}
